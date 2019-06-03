23518411bf7a3e3d882803c26ba637efb15b4408
Danial Wafiy <danialwafiy1014@gmail.com>
2019-06-03 16:01:29 +0800

    added sweet alert

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -39,12 +39,10 @@
     </div>
 
     <footer class="page-footer font-small blue">
-
-<div class="footer-copyright text-center py-3">© 2019 Developed By :
-  <a href="https://www.facebook.com/danialwafiy1014">Danial Wafiy</a>
-</div>
-
-</footer>
+      <div class="footer-copyright text-center py-3">© 2019 Developed By :
+        <a href="https://www.facebook.com/danialwafiy1014">Danial Wafiy</a>
+      </div>
+  </footer>
 
 </div>
 
